19bc7482e79574f23eb35202428eaeb9dd4799ce
Erick <ereyes3@madisoncollege.edu>
2023-04-14 00:00:57 -0500

    feat(src/test): Adding Database. feat(src/test): Adding database.properties. feat(src/main/resources): Adding hibernate.cfg.xml. feat(src/test/resources): Adding hibernate.cfg.xml. feat(src/test/java): Adding ItineraryDaoTest. feat(src/test): Adding PropertiesLoader. feat(src/test/java): Adding UserDaoTest. git push origin main

---
+++ src/test/java/com/aiagent/persistence/UserDaoTest.java
@@ -0,0 +1,37 @@
+package com.aiagent.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void findByPropertyEqual() {
+    }
+}
